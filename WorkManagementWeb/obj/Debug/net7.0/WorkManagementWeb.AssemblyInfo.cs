1b4e71621447bc11f4f5826f53ddd2e483830678
samed tüysüz <49450735+samedtysz53@users.noreply.github.com>
2023-12-07 16:06:28 +0300

    update
    update

--- WorkManagementWeb/obj/Debug/net7.0/WorkManagementWeb.AssemblyInfo.cs
+++ WorkManagementWeb/obj/Debug/net7.0/WorkManagementWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkManagementWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c50ddd12eda4bb969518e6d2accc076adacbd82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkManagementWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkManagementWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
